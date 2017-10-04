4d419c0d85963833fd97674780ed4f0e01acb0ab
Mayada Mahsoub <mayada.mahsoub@icstars-columbus.org>
2017-10-04 15:10:04 -0400

    working on improvement and safety

---
+++ AEPdashboard/Controllers/improvementNsafetyController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AEPdashboard.Controllers
+{
+    public class ImprovementNSafetyController : Controller
+    {
+        // GET: improvementNsafety
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
