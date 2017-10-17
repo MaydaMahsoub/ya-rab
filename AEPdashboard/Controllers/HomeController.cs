939a55d04695beb4b8ad8e3ac53895cab1207cd1
Matthew Jones <matthew.jones@icstars-columbus.org>
2017-10-17 15:43:11 -0400

    added variable call outs to views

--- AEPdashboard/Controllers/HomeController.cs
+++ AEPdashboard/Controllers/HomeController.cs
@@ -6,29 +6,32 @@ using System.Web.Mvc;
 using System.Data;
 using System.Data.EntityClient;
 using System.Data.Objects;
+using AEPdashboard.Models;
+using System.Linq;
 
 namespace AEPdashboard.Controllers
 {
     public class HomeController : Controller
     {
+        AEPDB db = new AEPDB();
         public ActionResult Index()
         {
-            return View();
+            return View(db.SCORECARD_FACT.ToList());
         }
 
         public ActionResult IS()
         {
-            return View();
+            return View(db.SCORECARD_FACT.ToList());
         }
 
         public ActionResult Financial()
         {
-            return View();
+            return View(db.SCORECARD_FACT.ToList());
         }
 
         public ActionResult Socialactivity()   
         {
-            return View();
+            return View(db.SCORECARD_FACT.ToList());
         }
 
         
